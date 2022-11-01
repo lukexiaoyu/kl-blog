1d1e8a2bf7836a8df20f6bc82daeaf8e5980de65
lukexiao <814008000@163.com>
2022-11-02 07:58:12 +0800

    1

---
+++ src/components/Nav.vue
@@ -0,0 +1,24 @@
+<script setup>
+import {ref,reactive,computed} from 'vue'
+const adds=[
+    {name:'Home',link:'/'},
+    {name:'每日计划',link:'/everyday'},
+]
+</script>
+<template>
+<div>
+    <template v-for="x in adds">
+    <a :href="x.link">{{x.name}}</a>
+    </template>
+</div>
+</template>
+<style scoped>
+a{
+    margin: 1px;
+    text-decoration: none;
+}
+a:hover{
+    background-color: black;
+    color: white;
+}
+</style>
