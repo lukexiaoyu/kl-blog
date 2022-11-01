096ed6ccab87d59fc2f7be703726f6611b6fd4f7
lukexiao <814008000@163.com>
2022-11-01 15:36:03 +0800

    1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import deno from "@astrojs/deno";
+
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: deno()
+  adapter: deno(),
+  integrations: [vue()]
 });
